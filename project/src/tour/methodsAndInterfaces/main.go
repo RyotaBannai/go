d615916cb276292860502ad8d9450269f230eaec
Ryota Bannai <ryotala0528@gmail.com>
2020-12-14 22:39:35 +0900

    add: be aware of implementing interface when using pointer receiver.

--- project/src/tour/methodsAndInterfaces/main.go
+++ project/src/tour/methodsAndInterfaces/main.go
@@ -11,7 +11,7 @@ type Vertex struct {
 
 // methods -> レシーバ引数を伴う関数 methods != functions
 // Vertex 型の receiver を持つ Abs method
-func (v Vertex) Abs() float64 { // distance
+func (v *Vertex) Abs() float64 { // distance
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
@@ -25,6 +25,32 @@ func myMethod() {
 	v := Vertex{3, 4}    // 5
 	v.Scale(10)          // 自分自身を更新
 	fmt.Println(v.Abs()) // 50
+}
+
+// interface は'メソッドシグネチャ'の集まりで定義される
+type Abser interface {
+	Abs() float64
+}
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	} else {
+		return float64(f)
+	}
+}
+
+func letImplementInteraface() {
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+	v := Vertex{3, 4}
+	a = f  // a MyFloat implements Abser
+	a = &v // a *Vertex implements Abser
+
+	//a = v // Error! Abs メソッドは、Vertex ではなく、*Vertex の定義であるため.
+	fmt.Println(a.Abs())
 }
 
 func main() {
