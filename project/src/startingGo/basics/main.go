9db9e4f5b23a760a24dd80a7d85744762aec71d3
Ryota Bannai <ryotala0528@gmail.com>
2020-12-20 14:51:56 +0900

    add: range of String type

--- project/src/startingGo/basics/main.go
+++ project/src/startingGo/basics/main.go
@@ -5,6 +5,22 @@ import (
 	"math"
 )
 
+func loopString() {
+	for i, r := range "あいうえお" {
+		fmt.Printf("[%d] = %d\n", i, r)
+	}
+	/*
+		文字列に対する range は ulf-8 でエンコードされた文字列のコードポイントごとに反復される.
+		→ 第一変数の値はインデックスではなく、コードポインタが開始されるバイト列のインデックス.
+		→ 文字のコードポイントに応じて文字列のインデックスの増分は異なる.
+		[0] = 12354
+		[3] = 12356
+		[6] = 12358
+		[9] = 12360
+		[12] = 12362
+	*/
+}
+
 func main() {
 	a := []string{
 		"Michael",
@@ -13,4 +29,5 @@ func main() {
 	fmt.Println(a)
 	fmt.Println(pkgVar) // print package variable // go にはグローバル変数はない
 	checkedOverflow(1, math.MaxUint32)
+	loopString()
 }
