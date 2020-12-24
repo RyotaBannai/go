64dd4387e8039e591aaa2c0f3d903db842ded884
Ryota Bannai <ryotala0528@gmail.com>
2020-12-24 22:47:58 +0900

    add: how to make Map thread-safely.

---
+++ project/src/minna/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type KeyValue struct {
+	store map[string]string
+	mu    sync.RWMutex
+}
+
+func NewKeyValue() *KeyValue {
+	return &KeyValue{store: make(map[string]string)}
+}
+
+func (kv *KeyValue) Set(key, val string) {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	kv.store[key] = val
+}
+
+func (kv *KeyValue) Get(key string) (string, bool) {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	val, ok := kv.store[key]
+	return val, ok
+}
+
+func main() {
+	kv := NewKeyValue()
+	kv.Set("Key", "Value")
+	val, ok := kv.Get("Key")
+	if ok {
+		fmt.Println(val)
+	}
+}
