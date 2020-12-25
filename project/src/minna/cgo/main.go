25c07068a9e30b47888604bb03962f0a7b0d2c66
Ryota Bannai <ryotala0528@gmail.com>
2020-12-26 00:29:49 +0900

    add: cgo

---
+++ project/src/minna/cgo/main.go
@@ -0,0 +1,38 @@
+package main
+
+/*
+#include <stdio.h>
+#include <math.h>
+#define MSG "Hello"
+#define ADD(a,b) (a + b)
+void Cprint(char* str){
+	printf("%s", str);
+}
+
+double MyPow(double x, double y){
+	double ret= pow(x,y);
+	return ret;
+}
+
+int Add(int a, int b){
+	int ret = ADD(a, b);
+	return ret;
+}
+*/
+import "C"
+import "fmt"
+
+func main() {
+	str := C.CString("hoge\n")
+	// CString: golang の String 型を C 言語の char* に変換
+	C.Cprint(str)
+
+	ret := C.MyPow(10, 2)
+	fmt.Println(ret)
+
+	// マクロ
+	res := C.MSG
+	fmt.Println(res)
+	res2 := C.Add(10, 10)
+	fmt.Println(res2)
+}
