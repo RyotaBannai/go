a65896ca145281ed2a9fef630728891d66c8f3d4
Ryota Bannai <ryotala0528@gmail.com>
2020-12-29 00:10:19 +0900

    add: test same..

--- project/src/tour/exercise/binaryTree/main.go
+++ project/src/tour/exercise/binaryTree/main.go
@@ -24,7 +24,7 @@ func Walk(t *tree.Tree, ch chan int) {
 	go Walk(t.Right, ch)
 }
 
-func tester() {
+func testWalk1() {
 	ch := make(chan int)
 	t1 := tree.New(1)
 	go Walk(t1, ch)
@@ -40,10 +40,6 @@ func tester() {
 		}
 	}
 }
-
-//func Same(t1, t2, *tree.Tree) bool {
-//
-//}
 
 // 全部完了する前に close してしまう..
 // sub tree が close してしまうため　-> children には chan を渡さないようにする → Walk3
@@ -69,10 +65,43 @@ func Walk3(t *tree.Tree, ch chan int) {
 	close(ch)
 }
 
-func main() {
+func testWalk3() {
 	ch := make(chan int, 10)
-	go Walk3(tree.New(1), ch)
+	ch2 := make(chan int, 10)
+	Walk3(tree.New(1), ch)
+	Walk3(tree.New(1), ch2)
+	//for i := range ch {
+	//	fmt.Println(i)
+	//}
 	for i := range ch {
 		fmt.Println(i)
 	}
+	for i := range ch2 {
+		fmt.Println(i)
+	}
+}
+
+// 全く同じ構造の判定には使える
+// が構造は違くても同じ要素を含んでいる場合の判定には使えない...
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk3(t1, ch1)
+	go Walk3(t2, ch2)
+	for v1 := range ch1 {
+		v2, ok := <-ch2
+		if !ok || v1 != v2 {
+			return false
+		}
+	}
+	return true
+}
+
+func testSame() {
+	println("same 1,1", Same(tree.New(1), tree.New(1)))
+	println("same 1,2", Same(tree.New(1), tree.New(2)))
+}
+
+func main() {
+	testSame()
 }
