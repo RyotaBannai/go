ffb38f14382a1a19277eb63da979a4f0fb5796ce
Ryota Bannai <ryotala0528@gmail.com>
2020-12-28 23:58:58 +0900

    fix: Walk ver2

--- project/src/tour/exercise/binaryTree/main.go
+++ project/src/tour/exercise/binaryTree/main.go
@@ -24,11 +24,7 @@ func Walk(t *tree.Tree, ch chan int) {
 	go Walk(t.Right, ch)
 }
 
-//func Same(t1, t2, *tree.Tree) bool {
-//
-//}
-
-func main() {
+func tester() {
 	ch := make(chan int)
 	t1 := tree.New(1)
 	go Walk(t1, ch)
@@ -42,5 +38,41 @@ func main() {
 		case value := <-ch:
 			values = append(values, value)
 		}
+	}
+}
+
+//func Same(t1, t2, *tree.Tree) bool {
+//
+//}
+
+// 全部完了する前に close してしまう..
+// sub tree が close してしまうため　-> children には chan を渡さないようにする → Walk3
+func Walk2(t *tree.Tree, ch chan int) {
+	if t != nil {
+		ch <- t.Value
+		Walk2(t.Left, ch)
+		Walk2(t.Right, ch)
+	}
+	close(ch)
+}
+
+func Walk3(t *tree.Tree, ch chan int) {
+	var subWalk func(*tree.Tree)
+	subWalk = func(t *tree.Tree) {
+		if t != nil {
+			ch <- t.Value
+			subWalk(t.Left)
+			subWalk(t.Right)
+		}
+	}
+	subWalk(t)
+	close(ch)
+}
+
+func main() {
+	ch := make(chan int, 10)
+	go Walk3(tree.New(1), ch)
+	for i := range ch {
+		fmt.Println(i)
 	}
 }
