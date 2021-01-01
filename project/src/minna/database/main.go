dbefe78183ccd04d841daaa3a2da203e1198e8ba
Ryota Bannai <ryotala0528@gmail.com>
2021-01-01 23:54:38 +0900

    add: use postgres

---
+++ project/src/minna/database/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	_ "github.com/lib/pq"
+)
+
+type Slicer interface {
+	toSlice() []interface{}
+}
+
+type Person struct {
+	id   string
+	name string
+	age  int
+}
+
+func (p *Person) toSlice() []interface{} {
+	return []interface{}{p.id, p.name, p.age}
+}
+
+type queries interface {
+	selectAll() queries
+	insert() queries
+	update() queries
+	delete() queries
+	run() (interface{}, error)
+}
+
+type PeopleQueries struct {
+	currentQuery string
+	person       Person
+	db           *sql.DB
+}
+
+func (p *PeopleQueries) selectAll() queries {
+	p.currentQuery = `SELECT * FROM people;`
+	return p
+}
+
+func (p *PeopleQueries) insert() queries {
+	p.currentQuery = `INSERT INTO people(id, name, age) VALUES($1, $2, $3)`
+	return p
+}
+
+func (p *PeopleQueries) update() queries {
+	p.currentQuery = `UPDATE people where...`
+	return p
+}
+
+func (p *PeopleQueries) delete() queries {
+	p.currentQuery = `DELETE people where...`
+	return p
+}
+
+func (p *PeopleQueries) run() (interface{}, error) {
+	if p.currentQuery == "" {
+		return nil, errors.New("Needs to query string before run.\n")
+	}
+	result, err := p.db.Exec(p.currentQuery, p.person.toSlice()...)
+	//ri, err := result.LastInsertId()
+	//ra, err := result.RowsAffected()
+	//fmt.Printf("%v\n%v\n", ri, ra)
+	return result, err
+}
+
+func main() {
+	connStr := "postgres://ryota:bannai@localhost/gotest?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	peopleQuery := PeopleQueries{
+		person:       Person{"1006", "Mark", 35},
+		db:           db,
+		currentQuery: ""}
+	result, err := peopleQuery.insert().run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result)
+}
